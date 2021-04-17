b4d1bbe97b336ffd195840b092a0503e3d77f73e
dumbus <dumbusx@gmail.com>
2021-04-17 17:56:24 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__percents-counter'),
+        lines = document.querySelectorAll('.skills__percents-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML; 
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {    
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+//smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
